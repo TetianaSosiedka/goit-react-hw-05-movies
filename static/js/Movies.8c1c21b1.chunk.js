04d6380c9b6154a28946727fe0d8028d2206768c
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-08-05 19:24:54 +0000

    Deploying to gh-pages from @ TetianaSosiedka/goit-react-hw-05-movies@756850cf3613188f4c56c9beee9c4b75c855aac6 🚀

---
+++ static/js/Movies.8c1c21b1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[249],{3536:function(e,t,i){i.r(t);var s=i(184);t.default=function(){return(0,s.jsx)("h2",{children:"Movies Page"})}}}]);
+//# sourceMappingURL=Movies.8c1c21b1.chunk.js.map
